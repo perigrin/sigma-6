42ac4a646967ddfcd1bd69851b0b529642d409cd
Chris Prather <chris@prather.org>
2011-05-29 16:19:12 -0400

    simplify

---
+++ app.psgi
@@ -0,0 +1,5 @@
+#!/usr/bin/env perl
+use Sigma6;
+my $app = sub { Sigma6->run_psgi(@_) };
+
+__END__
--- lib/Sigma6.pm
+++ lib/Sigma6.pm
@@ -1,8 +1,36 @@
 package Sigma6;
-use Moose 1.01;
 our $VERISON = '0.01';
-use namespace::autoclean 0.09;
-use Plack 0.9929;
+use Plack 0.9979;
+
+sub _404 {
+    return [
+        404,
+        [ "Content-Type", "text/plain" ],
+        ["Sorry we're not sure how to handle that request"],
+    ];
+}
+
+sub _GET_index {
+    my $r = shift;
+    return 1 if $r->path eq '/' && $r->method eq 'GET';
+    return 0;
+}
+
+sub _POST_build {
+    my $r = shift;
+    return 1 if $r->path eq '/build' && $r->method eq 'POST';
+    return 0;
+}
+
+sub run_psgi {
+    my $self = shift;
+    return sub {
+        my $r = Plack::Request->new(shift);
+        return $self->index($r) if _GET_index($r);
+        return $self->build($r) if _POST_build($r);
+        return $self->_404($r);
+      }
+}
 
 1;
 __END__
