6c7be64bff13619bf111e982ffa3e96781ca623c
Chris Prather <chris@prather.org>
2012-02-05 13:11:32 -0500

    move to using a sharedir for templates

--- lib/Sigma6/Plugin/Template/Tiny.pm
+++ lib/Sigma6/Plugin/Template/Tiny.pm
@@ -2,15 +2,43 @@ package Sigma6::Plugin::Template::Tiny;
 use Moose;
 use namespace::autoclean;
 use Template::Tiny;
+use Try::Tiny;
+use File::ShareDir qw(class_file);
 
 extends qw(Sigma6::Plugin);
 with qw(Sigma6::Plugin::API::RenderHTML);
+
+has build_template => (
+    is      => 'ro',
+    isa     => 'Str',
+    lazy    => 1,
+    builder => '_build_template',
+);
+
+sub _build_template {
+    my $file = try { class_file( __PACKAGE__, 'root/src/build.tt' ) }
+    catch {'share/root/src/build.tt'};
+    return do { local $/; open my $fh, '<', $file; <$fh>};
+}
+
+has all_builds_template => (
+    is      => 'ro',
+    isa     => 'Str',
+    lazy    => 1,
+    builder => '_all_builds_template',
+);
+
+sub _all_builds_template {
+    my $file = try { class_file( __PACKAGE__, 'root/src/all_builds.tt' ) }
+    catch {'share/root/src/all_builds.tt'};
+    return do { local $/; open my $fh, '<', $file; <$fh>};
+}
 
 sub render_build {
     my ( $self, $r, $builds ) = @_;
     my $vars = { o => $self, b => $builds->[0] };
     Template::Tiny->new->process(
-        ( $self->build_template, $vars ) => \( my $output ) );
+        ( \$self->build_template, $vars ) => \( my $output ) );
     return $output;
 }
 
@@ -18,7 +46,7 @@ sub render_all_builds {
     my ( $self, $r, $builds ) = @_;
     my $vars = { o => $self, builds => $builds };
     Template::Tiny->new->process(
-        ( $self->all_builds_template, $vars ) => \( my $output ) );
+        ( \$self->all_builds_template, $vars ) => \( my $output ) );
     return $output;
 }
 
@@ -34,58 +62,7 @@ sub target {
 
 sub repo_plugins {
     my $self = shift;
-    [$self->plugins_with('-Repository')];
-}
-
-sub build_template {
-    return \qq[
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <title>Sigma6</title>
-        </head>
-        <body>
-            <h1>[% o.target %]</h1>
-            <h2>Build [% b.id %]</h2>
-            <p><i>[% b.description %]</i></p>
-            <form action="/" method="POST"><input type="submit" value="Build"/></form>
-            <div><pre><code>[% b.status %]</code></pre></div>
-        </body>
-    </html>
-    ];
-}
-
-sub all_builds_template {
-    return \qq[
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Sigma6</title>
-    </head>
-    <body>
-    <h1>Sigma6 Builds</h1>
-    [%- UNLESS builds.count %]
-        <p>No Builds Yet</p>
-    [%- ELSE -%]
-        [% FOREACH b IN builds %]
-            <h2>Build [% b.id %]</h2>
-            <p><i>[% b.description %]</i></p>
-            <form action="/" method="POST"><input type="submit" value="Build"/></form>
-            <div><pre><code>[% b.status %]</code></pre></div>
-        [% END %]
-    [% END %]
-    <form method='POST' action="/">
-        <select name="some_name" id="some_name" onchange="" size="1">
-            [%- FOREACH plugin IN o.repo_plugins %]
-            <option value="[% plugin.name %]">[% plugin.name %]</option>
-            [%- END %]
-        </select>
-        <input type="text" value="target" />
-        <input type="submit">
-    </form>
-    </body>
-</html>
-]
+    [ $self->plugins_with('-Repository') ];
 }
 
 1;
