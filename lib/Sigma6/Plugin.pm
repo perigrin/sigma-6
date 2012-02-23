0369c2f95d7fe436b5baca3f73bd7025ae911e11
Chris Prather <chris@prather.org>
2012-02-23 01:08:38 -0500

    unify logging methods

--- lib/Sigma6/Plugin.pm
+++ lib/Sigma6/Plugin.pm
@@ -11,30 +11,69 @@ has config => (
     handles  => 'Sigma6::Config',
 );
 
-sub name {
-    my ($name) = shift->meta->name =~ m/::(\w+)$/;
-    return $name;
-}
-
 sub log {
-        my ($self, $level, $message) = @_;
-        for ($self->plugins_with('-Logger')) {
-                $_->$level($message);
-        }
+    my ( $self, $level, $message ) = @_;
+    for ( $self->plugins_with('-Logger') ) {
+        $_->$level($message);
+    }
 }
 
-sub warn {
-    shift->log(warn => @_);
-}
-
-sub die {
-    shift->log(die => @_);
-}
+sub warn { shift->log( warn => @_ ) }
+sub die  { shift->log( die  => @_ ) }
 
 __PACKAGE__->meta->make_immutable;
 1;
 __END__
 
 
-=head1 NAME Sigma6::Plugin
+=head1 NAME 
 
+Sigma6::Plugin
+
+=head1 SYNOPSIS
+
+    package Sigma6::Plugin::AwesomeNess
+    use Moose;
+    
+    extends qw(Sigma6::Plugin);
+    
+    ... 
+    
+    1;
+    __END__
+
+=head1 DESCRIPTION
+
+This is the base class for Sigma6::Plugins. It exists basically to sit at the
+top of the plugin tree. It does provide some basic shared behavior for all
+plugins.
+
+=head1 ATTRIBUTES
+
+=over 4 
+
+=item config
+
+This holds the L<Sigma6::Config> object and delegates the L<Sigma6::Config>
+API.
+
+=back
+
+=head1 METHODS
+
+=over 4
+
+=item log ($level, $message)
+
+Calls C<$logger->$level($message)> for each C<Logger> configured in the
+system.
+
+=item warn ($message)
+
+A convience wrapper for C<$self->log( warn => $message)>
+
+=item die ($message)
+
+A convience wrapper for C<$self->log( die => $message)>
+
+=back
--- lib/Sigma6/Plugin/API/Logger.pm
+++ lib/Sigma6/Plugin/API/Logger.pm
@@ -2,9 +2,55 @@ package Sigma6::Plugin::API::Logger;
 use Moose::Role;
 use namespace::autoclean;
 
-# ABSTRACT: Turn baubles into trinkets
+# ABSTRACT: Logging API
 
-requires qw(logger warn die);
+requires qw(
+    logger
+    trace
+    debug
+    notice
+    warn
+    die
+);
 
 1;
 __END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+This is a base API for a Logging plugin.
+
+=head1 REQUIRED METHODS
+
+=over 4
+
+=item logger() : Logger
+
+Return the actual Logger instance
+
+=item trace($message) 
+
+Log something at a "trace" level. Trace in these terms is a mild form of
+debugging used to make sure everything is flowing properly.
+
+=item debug($message)
+
+Log something at a "debug" level. This a more useful level than trace and is
+typically used for digging out specific issues.
+
+=item notice
+
+Log something at a "notice" level. Notifications are for non-exception
+logging.
+
+=item warn
+
+Log something at a 'warn' level. Warn is for non-fatal exception logging.
+
+=item die
+
+Log something at a 'die' level. Die is for fatal exception logging.
+
+=back
